08f122fed1374c112ae1c6a76c34766053a9d96d
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2022-01-13 02:03:39 +0530

    console

--- practice.ts
+++ practice.ts
@@ -1,8 +1,1 @@
-const input1 = document.getElementById('num1')! as HTMLInputElement
-const input2 = document.getElementById('num2')! as HTMLInputElement
-const btnAdd = document.getElementById('btnAdd')! as HTMLButtonElement
-
-const addition = (num1: number, num2:number) => {
-    const numInp1 = document.getElementById('num1')
-}
-
+console.log("hello typeScript")
