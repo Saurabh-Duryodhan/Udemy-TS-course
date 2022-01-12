ea32e04601a2001b168875e76206da36f1b6dff2
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2022-01-13 02:00:56 +0530

    first commit for udemy ts-course

---
+++ practice.ts
@@ -0,0 +1,8 @@
+const input1 = document.getElementById('num1')! as HTMLInputElement
+const input2 = document.getElementById('num2')! as HTMLInputElement
+const btnAdd = document.getElementById('btnAdd')! as HTMLButtonElement
+
+const addition = (num1: number, num2:number) => {
+    const numInp1 = document.getElementById('num1')
+}
+
